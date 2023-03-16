bcc6e837f27679f435e9bdf61158050c74205960
vasiliy <vasya-timkin@mail.ru>
2023-03-16 18:54:53 +0300

    Задание13

---
+++ zadanie13.cs
@@ -0,0 +1,13 @@
+﻿//Задача 13 
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine(anyNumberText[2]);
+}
+else 
+{
+  Console.WriteLine("третьей цитфры не");
+}
